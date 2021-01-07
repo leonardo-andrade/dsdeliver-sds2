e564c7de8e8bcb6fd37d96bb9e6d1616156b56b6
unknown <leonardogsandrade@gmail.com>
2021-01-07 18:51:35 -0300

    OrderDTO / erro no git

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsdeliver.entities.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
