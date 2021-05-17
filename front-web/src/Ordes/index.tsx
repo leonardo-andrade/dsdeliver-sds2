3ce328cdb29870f03a3cb8cde7dbb4142b7ddcea
unknown <leonardogsandrade@gmail.com>
2021-05-17 02:58:06 -0300

    antes da Location

--- front-web/src/Ordes/index.tsx
+++ front-web/src/Ordes/index.tsx
@@ -1,11 +1,27 @@
-import StepsHeader from './StepsHeaders';
+import { useEffect, useState } from 'react';
 import './styles.css';
+import StepsHeader from './StepsHeaders';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
+function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
 
-function Orders(){
-    return(
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
+            <OrderLocation />
+
         </div>
     )
 }
