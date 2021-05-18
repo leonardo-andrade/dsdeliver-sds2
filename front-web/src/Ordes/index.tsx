bb6ce41c2086ef54d1be3f279f55ffde59b21a0c
unknown <leonardogsandrade@gmail.com>
2021-05-18 02:14:32 -0300

    Search order location, leaflet bugado

--- front-web/src/Ordes/index.tsx
+++ front-web/src/Ordes/index.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeaders';
 import ProductsList from './ProductsList';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
-    console.log(products);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>(); 
 
     useEffect(() => {
         fetchProducts()
@@ -20,7 +20,7 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products} />
-            <OrderLocation />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
 
         </div>
     )
