000320bcd7c5679dbdda5d9d090d985116daf981
unknown <leonardogsandrade@gmail.com>
2021-05-18 03:30:11 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,15 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routers from './Routers';
 
 function App() {
   return (
+    <>
     <Routers />
+    <ToastContainer />
+    </>
   );
 }
 
