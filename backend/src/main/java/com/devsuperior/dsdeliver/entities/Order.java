500273d2b6d53971e2bf0d47b002b8f041484051
unknown <leonardogsandrade@gmail.com>
2021-05-18 22:25:17 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -29,9 +29,7 @@ public class Order implements Serializable {
 	private OrderStatus status;
 
 	@ManyToMany
-	@JoinTable(name = "tb_order_product", 
-		joinColumns = @JoinColumn(name = "order_id"), 
-		inverseJoinColumns = @JoinColumn(name = "product_id"))
+	@JoinTable(name = "tb_order_product", joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
 
 	public Order() {
@@ -94,6 +92,14 @@ public class Order implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
 	}
 
 	public Set<Product> getProducts() {
